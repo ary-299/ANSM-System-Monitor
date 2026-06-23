1c898ca408c80eddc44ea6adc7acf81c49462bb1
ary-299 <plsenjoyosu@gmail.com>
2026-06-23 21:38:02 +0200

    added wifi connection and disk space TODO: fix multiple wifi bugs and clearing bug

--- display.py
+++ display.py
@@ -1,28 +1,76 @@
 
 from colorama import Fore, Back, Style, init
+import yaml
+
+config_path = "cfg.YAML"
+with open(config_path, "r", encoding="utf-8") as f:
+    cfg = yaml.safe_load(f)
+
+thresholds = cfg.get("thresholds", {})
+
+cpu_warn_tresh = int(thresholds.get("cpu_warn_tresh", 80))
+ram_warn_tresh = int(thresholds.get("ram_warn_tresh", 80)) 
+cpu_crit_tresh = int(thresholds.get("cpu_crit_tresh", 95))
+ram_crit_tresh = int(thresholds.get("ram_crit_tresh", 95))
+disk_warn_tresh = int(thresholds.get("disk_warn_tresh", 85))
+disk_crit_tresh = int(thresholds.get("disk_crit_tresh", 95))
+download_warn_tresh = int(thresholds.get("download_warn_tresh", 50))
+download_crit_tresh = int(thresholds.get("download_crit_tresh", 90))
+upload_warn_tresh = int(thresholds.get("upload_warn_tresh", 50))
+upload_crit_tresh = int(thresholds.get("upload_crit_tresh", 90))
 
 def show_header():
     print(Fore.RED+Style.BRIGHT+"ANSM "+Style.RESET_ALL+Style.BRIGHT+"Panel\n")
 
 def print_cpu(cpu_val):         #setting "def" commands to run them on "main" later in the script
-    if cpu_val<50:
+    if cpu_val< cpu_warn_tresh:
         print(Style.BRIGHT+"CPU usage: " + Style.RESET_ALL + Style.BRIGHT + Fore.GREEN + f" {cpu_val}% " + Style.RESET_ALL)   #prints green for good values
-    elif cpu_val<77:
+    elif cpu_val<cpu_warn_tresh and cpu_val<cpu_crit_tresh:
         print(Style.BRIGHT+"CPU usage: " + Style.RESET_ALL + Style.BRIGHT + Fore.YELLOW + f" {cpu_val}% " + Style.RESET_ALL)  #prints yellow for okay values
-    elif cpu_val<85:
+    elif cpu_val<cpu_crit_tresh and cpu_val<cpu_warn_tresh:
         print(Style.BRIGHT +"CPU usage: " + Style.RESET_ALL + Style.BRIGHT + Fore.RED + f" {cpu_val}% " + Style.RESET_ALL)   #prints red for bad values
     elif cpu_val>=95:
         print(Style.BRIGHT +"CPU usage: " + Style.RESET_ALL + Style.BRIGHT + Fore.RED + f" {cpu_val}% ! ! !" + Style.RESET_ALL)  #prints red with three exl. marks for horrible values
 
 def print_ram(ram_val):         #setting "def" commands to run them on "main" later in the script
-    if ram_val<50:
+    if ram_val<ram_warn_tresh:
         print(Style.BRIGHT +  "RAM usage: " + Style.RESET_ALL + Style.BRIGHT + Fore.GREEN + f" {ram_val}% " + Style.RESET_ALL) #check cpu values
-    elif ram_val<80:
+    elif ram_val<ram_crit_tresh and ram_val<ram_warn_tresh:
         print(Style.BRIGHT+"RAM usage: " + Style.RESET_ALL + Style.BRIGHT + Fore.YELLOW + f" {ram_val}% " + Style.RESET_ALL)
-    elif ram_val<90:
+    elif ram_val<ram_crit_tresh:
         print(Style.BRIGHT +"RAM usage: " + Style.RESET_ALL + Style.BRIGHT + Fore.RED + f" {ram_val}% " + Style.RESET_ALL)
     elif ram_val>=95:
         print(Style.BRIGHT +"RAM usage: " + Style.RESET_ALL + Style.BRIGHT + Fore.RED + f" {ram_val}% ! ! !" + Style.RESET_ALL)
 
+def print_download(wifi_recv):
+    if wifi_recv<download_warn_tresh:
+        print(Style.BRIGHT +  "Download Speed: " + Style.RESET_ALL + Style.BRIGHT + Fore.GREEN + f" {wifi_recv}GB " + Style.RESET_ALL) #check download values
+    elif wifi_recv<download_warn_tresh and wifi_recv<download_crit_tresh:
+        print(Style.BRIGHT+"Download Speed: " + Style.RESET_ALL + Style.BRIGHT + Fore.YELLOW + f" {wifi_recv}GB " + Style.RESET_ALL)
+    elif wifi_recv<download_crit_tresh:
+        print(Style.BRIGHT +"Download Speed: " + Style.RESET_ALL + Style.BRIGHT + Fore.RED + f" {wifi_recv}GB" + Style.RESET_ALL)
+    elif wifi_recv>=1:
+        print(Style.BRIGHT +"Download Speed: " + Style.RESET_ALL + Style.BRIGHT + Fore.RED + f" {wifi_recv}GB ! ! !" + Style.RESET_ALL)
+
+def print_upload(wifi_sent):
+    if wifi_sent<download_warn_tresh:
+        print(Style.BRIGHT +  "Upload Speed: " + Style.RESET_ALL + Style.BRIGHT + Fore.GREEN + f" {wifi_sent}GB " + Style.RESET_ALL) #check download values
+    elif wifi_sent<upload_warn_tresh and wifi_sent<upload_crit_tresh:
+        print(Style.BRIGHT+"Upload Speed: " + Style.RESET_ALL + Style.BRIGHT + Fore.YELLOW + f" {wifi_sent}GB " + Style.RESET_ALL)
+    elif wifi_sent<upload_crit_tresh:
+        print(Style.BRIGHT +"Upload Speed: " + Style.RESET_ALL + Style.BRIGHT + Fore.RED + f" {wifi_sent}GB " + Style.RESET_ALL)
+    elif wifi_sent>=1:
+        print(Style.BRIGHT +"Upload Speed: " + Style.RESET_ALL + Style.BRIGHT + Fore.RED + f" {wifi_sent}GB ! ! !" + Style.RESET_ALL)
+    
+
+def print_disk(disk_prec):
+    if disk_prec < disk_warn_tresh:
+        print(Style.BRIGHT + "Disk usage:"+Style.RESET_ALL + Style.BRIGHT+ Fore.GREEN +f"{disk_prec}%" + Style.RESET_ALL)
+    if disk_prec > disk_warn_tresh and disk_prec < disk_crit_tresh:
+        print(Style.BRIGHT + "Disk usage:"+Style.RESET_ALL + Style.BRIGHT+ Fore.YELLOW +f"{disk_prec}%" + Style.RESET_ALL)
+    if disk_prec > disk_crit_tresh and disk_prec > 97:
+        print(Style.BRIGHT + "Disk usage:"+Style.RESET_ALL + Style.BRIGHT+ Fore.RED +f"{disk_prec}%" + Style.RESET_ALL)
+    if disk_prec > 97:
+        print(Style.BRIGHT + "Disk usage:"+Style.RESET_ALL + Style.BRIGHT+ Fore.RED +f"{disk_prec}%! ! !" + Style.RESET_ALL)
 def show_footer():
     print("\nmade by ary")
--- main.py
+++ main.py
@@ -30,28 +30,29 @@ while True:
     ram = psutil.virtual_memory().percent # getting ram percentage and setting it as the "ram" variable
     Cused = psutil.disk_usage("C://").used
     Ctotal = psutil.disk_usage("C://").total
-    Cprec = psutil.disk_usage("C://").percent
+    Cprec_unclean = psutil.disk_usage("C://").percent
+    Cprec = round(Cprec_unclean, 1)
     Cfree = Ctotal - Cused
 
     Wifisent = psutil.net_io_counters().bytes_sent
     bytes_changed_sent = Wifisent - old_bytes_sent
     unclean_speed_sent = bytes_changed_sent / 1024/ interval
-    speed_sent = round(unclean_speed_sent, 2)
+    speed_sent = round(unclean_speed_sent, 1)
     old_bytes_sent = Wifisent
 
     Wifirecv = psutil.net_io_counters().bytes_recv
     bytes_changed_recv = Wifirecv - old_bytes_recv
     unclean_speed_recv = bytes_changed_recv / 1024 / interval
-    speed_recv = round(unclean_speed_recv, 2)
+    speed_recv = round(unclean_speed_recv, 1)
     old_bytes_recv = Wifirecv
     
     os.system('cls' if os.name == 'nt' else 'clear')
     display.show_header() 
     display.print_cpu(cpu)      # taking the cpu tracking number and sending it over to display.py
     display.print_ram(ram)      # same as above but with the live ram intake data
-    print(speed_sent)
-    print(speed_recv)
-    print(Cprec)
+    display.print_download(speed_recv)
+    display.print_upload(speed_sent)
+    display.print_disk(Cprec)
     display.show_footer() 
     
     # passing the live system numbers AND the limits directly into safety.py so it can process them instantly
